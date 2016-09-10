950dfea5bf179c4111b2f617d3619c0f5aca3b6a
albertusk95 <albertusk15@gmail.com>
2016-09-10 19:58:20 +0700

    new route for add user

--- nodemongo0/routes/index.js
+++ nodemongo0/routes/index.js
@@ -3,12 +3,50 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Simple Node.js and MongoDB App' });
 });
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({}, {}, function(e, docs) {
+        res.render('userlist', {userlist : docs });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', {title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set the internal DB variable
+    var db = req.db;
+
+    // Get the form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set the collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // Else, forward to success page
+            res.redirect("userlist");
+        }
+    });
 });
 
 module.exports = router;
